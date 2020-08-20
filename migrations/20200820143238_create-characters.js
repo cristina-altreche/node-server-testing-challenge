57bf011a6f054855feab6e771ab84272bc77936b
Cristina Altreche <c.altreche@gmail.com>
2020-08-20 14:35:14 -0400

    migrations

--- migrations/20200820143238_create-characters.js
+++ migrations/20200820143238_create-characters.js
@@ -1,8 +1,11 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("characters", (tbl) => {
+    tbl.increments();
+    tbl.string("characterName").unique().notNullable();
+    tbl.string("class").notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists("characters");
 };
