7cefd93a5f81dd98cc6a494f50bef5ee31fcbd22
Cristina Altreche <c.altreche@gmail.com>
2020-08-21 09:13:59 -0400

    Added comments

--- characters/characters-model.js
+++ characters/characters-model.js
@@ -1,9 +1,30 @@
-const db = require('../data/db-config')
+const db = require("../data/db-config");
 
 module.exports = {
-    getCharacters
-}
+  getCharacters,
+  remove,
+  findById,
+  add,
+};
 
 function getCharacters(char) {
-    return db('characters')
+  return db("characters");
+}
+
+function remove(id) {
+  return db("characters").where({ id }).del();
+}
+
+function findById(id) {
+  return db("characters").where({ id }).first();
+}
+
+async function add(char) {
+  return db("characters")
+    .insert(char)
+    .returning("id")
+    .then((ids) => {
+      const id = ids[0];
+      return findById(id);
+    });
 }
--- characters/characters-router.test.js
+++ characters/characters-router.test.js
@@ -1,26 +1,70 @@
-const request = require('supertest')
-const server = require('../app/server')
+const request = require("supertest");
+const server = require("../app/server");
+const db = require("../data/db-config");
 
-describe('characters router', function() {
-    it('should run the tests', function() {
-        expect(true).toBe(true)
+describe("characters router", function () {
+  it("should run the tests", function () {
+    expect(true).toBe(true);
+  });
+});
+
+/// SHOULD RESPOND WITH 200
+describe("GET /api/characters", function () {
+  it("should respond with status 200", function () {
+    return request(server)
+      .get("/api/characters")
+      .then((res) => {
+        expect(res.status).toBe(200); //CHANGE TO SEE ERROR
+      });
+  });
+/// SHOULD RETURN AN ARRAY 
+  it("should return an array of characters", function () {
+    return request(server)
+      .get("/api/characters")
+      .then((res) => {
+        expect(Array.isArray(res.body)).toBe(true);
+      });
+  });
+});
+
+/// ABLE TO POST CHARACTER
+describe("characters router", () => {
+    beforeEach(async () =>{
+        // empty table and reset primary key back to 1
+        await db("characters").truncate();
     })
+    describe("POST /api/characters", () => {
+        it("should add character", async () => {
+            const res = await request(server).post("/api/characters").send({
+                characterName: "Esther",
+                class: "Mage"
+            });
+
+            const characters = await db("characters");
+
+            expect(characters).toHaveLength(1);
+
+            expect(res.status).toBe(201)
+
+        })
+    })
+
+
+/// ABLE TO DELETE CHARACTER 
+describe("DELETE /api/characters/:id", () => {
+  it("should delete character", async () => {
+    await request(server).post("/api/characters").send({
+      characterName: "Esther",
+      class: "Paladin"
+    });
+
+    const res = await request(server).delete("/api/characters/1").send();
+
+    const characters = await db("characters");
+
+    expect(characters).toHaveLength(0);
+
+    expect(res.status).toBe(204);
+  });
 })
-
-describe('GET /api/characters', function() {
-    it('should respond with status 200', function(){
-        return request(server)
-        .get('/api/characters')
-        .then(res => {
-            expect(res.status).toBe(200)//CHANGE TO SEE ERROR
-        })
-    })
-
-    it('should return an array of characters', function() {
-        return request(server)
-        .get('/api/characters')
-        .then(res => {
-            expect(Array.isArray(res.body)).toBe(true)
-        })
-    })
 })
